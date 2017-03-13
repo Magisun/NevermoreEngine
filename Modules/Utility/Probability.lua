4a85bfb3278197107f46a3985b33376108647cb5
Jean-Claude Paquin <jcrpaquin@yahoo.com>
2017-03-13 19:10:29 -0400

    Update doc comments
    Add some additional documentation comments, clean up existing ones.

--- Modules/Utility/Probability.lua
+++ Modules/Utility/Probability.lua
@@ -4,16 +4,23 @@
 local lib = {}
 
 function lib.BoxMuller()
-	--- Normal curve. [-1, 1]
+	-- Box-Muller Transform
+	-- Samples a normal distribution of mean=0, stddev=1 using a uniform random source
 	
-    return math.sqrt(-2 * math.log(math.random())) * math.cos(2 * math.pi * math.random()) / 2
+	return math.sqrt(-2 * math.log(math.random())) * math.cos(2 * math.pi * math.random()) / 2
 end
 
 function lib.UnboundedNormalDistribution(Average, StdDeviation)
+	-- Samples from a normal distribution with specified mean, and standard deviation
+	-- Note: Has no hard cutoffs, see NormalDistribution(...)
+	
 	return Average + lib.BoxMuller() * StdDeviation
 end
 
 function lib.NormalDistribution(Average, StdDeviation, HardMin, HardMax)
+	-- Samples from a normal distribution with specified mean, standard deviation, and cutoffs
+	-- Output is bounded by HardMin and HardMax
+	
 	return math.min(HardMax, math.max(HardMin, Average + lib.BoxMuller() * StdDeviation))
 end
 
