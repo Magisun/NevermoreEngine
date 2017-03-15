4f59b06f6f4330d6967973bab52273d6eb583570
Jean-Claude Paquin <jcrpaquin@yahoo.com>
2017-03-15 05:22:32 -0400

    Add Gamma single-sampler
    Adds a single-sample function for the Gamma family of distributions using K+Theta parameterization.
    
    I validated the prototypes for these in R, and they should work, but I'm not certain that the added complexity of switching between two sampling methods is worth it. Someone should run some profiles, but it probably won't be me just yet.

--- Modules/Utility/Probability.lua
+++ Modules/Utility/Probability.lua
@@ -26,6 +26,66 @@ function lib.NormalDistribution(Average, StdDeviation, HardMin, HardMax)
 	return math.min(HardMax, math.max(HardMin, Average + BoxMuller() * StdDeviation))
 end
 
+function lib.GammaDistribution(K, Scale)
+	-- Samples from a gamma distribution with specified alpha/k value and theta (scale) value
+	-- Note: To use alpha+beta parameters, theta=1/beta
+	
+	assert(K >= 0, "K must be greater than or equal to 0.")
+	
+	if K == 0 then
+		return 0
+	elseif K == 1 then
+		return -math.log(math.random())
+	elseif K < 1 then
+		-- Ahrens-Dieter acceptance–rejection method, doi:10.1145/358315.358390
+		val = 0
+		
+		while true do
+			u = math.random()
+			v = math.random()
+			w = math.random()
+			
+			if u <= math.exp(1)/(math.exp(1) + K) then
+				val = v^(1/K)
+				
+				if w * val^(K-1) <= val^(k-1) * math.exp(-val) then
+					break
+				end
+			else
+				val = 1 - math.log(v)
+				
+				if w * math.exp(-val) <= val^(k-1) * math.exp(-val) then
+					break
+				end
+			end
+		end
+		
+		return val*Scale
+	elseif K > 1 then
+		-- Marsaglia-Tsang transformation-rejection method, doi:10.1145/358407.358414
+		D = K - 1/3
+		C = 1/math.sqrt(9*D)
+		val = 0
+		
+		while true do
+			v = 0
+			while v <= 0 do
+				x = BoxMuller()
+				v = 1 + C*x
+			end
+			v = v^3
+			
+			u = math.random()
+			if u < 1 - 0.0331*x^4 or math.log(u) < 0.5*x^2 + d*(1 - v + math.log(v)) then
+				val = D*v
+				break
+			end
+		end
+		
+		return val*Scale
+	end
+end
+
 --- Discrete distributions
 
 function lib.Bernoulli(Probability)
